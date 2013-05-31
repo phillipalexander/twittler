5c9510ddfa9c8b74ad9a617bd02516fc7df84ed2
phillipalexander <git@phillipalexander.io>
2013-05-30 18:07:11 -0700

    move js from data_generator.js into twitter.js and remove redundant code

--- js/twittler.js
+++ js/twittler.js
@@ -1,94 +1,102 @@
-// // Sweet animated logo
-// jQuery(document).ready(function ($) {
-// 	$('.logo').addClass('animated fadeInUp');
-// 	var robotAngrify = function () {
-// 		console.log('robot=ANGRY');
-// 	};
-// 	$('.logo').mouseenter(robotAngrify);
-// });
-
 $(document).ready(function () {
+
+	//swoop the robot logo in on load
 	$('.logo').addClass('animated fadeInUp');
+
+	// bounce the robot logo on hover
 	$('.logo').hover(function () {
 		$('.logo').removeClass('animated fadeInUp');
 		$('.logo').addClass('animated bounce');
 	}, function () {
 		$('.logo').removeClass('animated bounce');
 	});
+
+	// var stream = {
+	// 	home: [],
+	// 	user: {
+	// 		shawndrost: [],
+	// 		sharksforcheap: [],
+	// 		mracus: [],
+	// 		douglascalhoun: []
+	// 	}
+	// };
+	// var users = Object.keys(stream.user);
+
+	// set up data structures
+	streams = {};
+	streams.home = [];
+	streams.users = {};
+	streams.users.shawndrost = [];
+	streams.users.sharksforcheap = [];
+	streams.users.mracus = [];
+	streams.users.douglascalhoun = [];
+	// users = Object.keys(streams.users);
+
+	// utility function for adding tweets to our data structures
+	var addTweet = function (newTweet) {
+		var username = newTweet.user;
+		streams.users[username].push(newTweet);
+		streams.home.push(newTweet);
+	};
+
+	// utility function
+	var randomElement = function (array) {
+		var randomIndex = Math.floor(Math.random() * array.length);
+		return array[randomIndex];
+	};
+
+	// random tweet generator
+	var users = Object.keys(streams.users);
+	var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
+	var verbs = ['drank', 'drunk', 'deployed', 'got', 'developed', 'built', 'invented', 'experienced', 'fought off', 'hardened', 'enjoyed', 'developed', 'consumed', 'debunked', 'drugged', 'doped', 'made', 'wrote', 'saw'];
+	var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
+	var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
+	var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '', '', '', ''];
+
+	var randomMessage = function () {
+		return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
+	};
+
+	// generate random tweets on a random schedule
+	var generateRandomTweet = function () {
+		var tweet = {};
+		tweet.user = randomElement(users);
+		tweet.message = randomMessage();
+		tweet.created_at = new Date();
+		addTweet(tweet);
+	};
+
+	for (var i = 0; i < 10; i++) {
+		generateRandomTweet();
+	}
+
+	var scheduleNextTweet = function () {
+		generateRandomTweet();
+		setTimeout(scheduleNextTweet, Math.random() * 1500);
+	};
+	scheduleNextTweet();
+
+	// utility function for letting students add "write a tweet" functionality
+	// (note: not used by the rest of this file.)
+	var writeTweet = function (message) {
+		if (!visitor) {
+			throw new Error('set the global visitor property!');
+		}
+		var tweet = {};
+		tweet.user = visitor;
+		tweet.message = message;
+		addTweet(tweet);
+	};
+
+	var $body = $('body');
+
+	var index = streams.home.length - 1;
+	while (index >= 0) {
+		var tweet = streams.home[index];
+		var $tweet = $('<div></div>');
+		$tweet.text('@' + tweet.user + ': ' + tweet.message);
+		$tweet.appendTo($body);
+		index -= 1;
+	}
+
 });
-
-/*===================================
-=            animate.css            =
-===================================*/
-
-// $(document).ready(function () {
-// 	$('.checks :checkbox').attr('checked', true);
-// 	$('.checks label').hover(function () {
-// 		var anim = $(this).html();
-// 		$(this).addClass(anim);
-// 	}, function () {
-// 		$(this).removeClass();
-// 	});
-// });
-
-/*-----  End of animate.css  ------*/
-
-/*==================================================
-=            original twittler goodness            =
-==================================================*/
-
-// Original Twittler goodness
-// $(document).ready(function () {
-// 	debugger;
-// 	var $body = $('body');
-// 	// $body.html('');
-
-// 	var index = streams.home.length - 1;
-// 	while (index >= 0) {
-// 		var tweet = streams.home[index];
-// 		var $tweet = $('<div></div>');
-// 		$tweet.text('@' + tweet.user + ': ' + tweet.message);
-// 		$tweet.appendTo($body);
-// 		index -= 1;
-// 	}
-// });
-
-/*-----  End of original twittler goodness  ------*/
-
-/*=====================================
-=            JQuery table?            =
-=====================================*/
-
-// Trying to build a table with JQuery
-//
-// $(document).ready(function () {
-// 	// create table
-// 	var $table = $('<table>');
-// 	// caption
-// 	// $table.append('<caption>MyTable</caption>')
-// 	// thead
-// 	.append('<thead>').children('thead')
-// 		.append('<tr />').children('tr').append('<th>A</th><th>B</th><th>C</th><th>D</th>');
-
-// 	//tbody
-// 	var $tbody = $table.append('<tbody />').children('tbody');
-
-// 	// add row
-// 	$tbody.append('<tr />').children('tr:last')
-// 		.append("<td>val</td>")
-// 		.append("<td>val</td>")
-// 		.append("<td>val</td>")
-// 		.append("<td>val</td>");
-
-// 	// add another row
-// 	$tbody.append('<tr />').children('tr:last')
-// 		.append("<td>val</td>")
-// 		.append("<td>val</td>")
-// 		.append("<td>val</td>")
-// 		.append("<td>val</td>");
-
-// 	// add table to dom
-// 	$table.appendTo('body');
-// });
-
-/*-----  End of JQuery table?  ------*/
